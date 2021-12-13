05be663e0db15417b119c46539433f857df373af
NATTQUEIROZ <nat.diasqueiroz@gmail.com>
2021-12-13 20:30:11 -0300

    cookenu - inicio do projeto

---
+++ modulo3/bloco11/cookenu/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddRecipesPage from ".pages/AddRecipesPage/AddRecipesPage/";
+
+const App = () => {
+  return (
+    <div>
+        <AddRecipesPage>
+          <RecipesLimport />
+    </div>
+  );
+}
+
+export default App;
