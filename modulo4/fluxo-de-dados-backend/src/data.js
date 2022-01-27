1df478c671b1a7cb4e9c5e14db26631e41160d7c
NATALIA DIAS QUEIROZ <nat.diasqueiroz@gmail.com>
2022-01-26 23:46:22 -0300

    atividades-fluxo-de-dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.js
@@ -0,0 +1,17 @@
+export const array = [
+    {
+        id: 1,
+        name: "Naruto Uzumaki",
+        price: 100
+    },
+    {
+        id: 2,
+        name: "Sasuke Uchiha",
+        price: 85
+    },
+    {
+        id: 3,
+        name: "Sakura Haruno",
+        price: 90
+    }
+]
