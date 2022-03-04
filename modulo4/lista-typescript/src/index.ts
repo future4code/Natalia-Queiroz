0171efa49234faa4cdb5fcd63062c036aeb98fbc
NATALIA DIAS QUEIROZ <nat.diasqueiroz@gmail.com>
2022-03-04 14:22:20 -0300

    lista ts - exercicio 1

--- modulo4/lista-typescript/src/index.ts
+++ modulo4/lista-typescript/src/index.ts
@@ -3,11 +3,7 @@ data de nascimento (ex.: “24/04/1993”). A função deve separar o dia, o mê
  e ano e retornar uma string no seguinte formato: */
 
  function infos (nome: string, data:string): string {
-    const dataSeparada = data.split("/")
-    const dia = dataSeparada[0]
-    const mes = dataSeparada[1]
-    const ano = dataSeparada[2]
-
-    const saudacoes = `Olá me chamo ${nome}, nasci no dia ${dia} do mês de ${mes} do ano de ${ano}`
+    const [dia, mes, ano] = data.split("/")
+    const saudacoes: string = `Olá me chamo ${nome}, nasci no dia ${dia} do mês de ${mes} do ano de ${ano}`
     return saudacoes
  }
