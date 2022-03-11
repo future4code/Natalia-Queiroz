35afc11439d76fd72aafd16af617c743f3a6639f
NATALIA DIAS QUEIROZ <nat.diasqueiroz@gmail.com>
2022-03-11 11:11:07 -0300

    database

--- modulo4/to-do-list/src/index.ts
+++ modulo4/to-do-list/src/index.ts
@@ -1,25 +1,31 @@
 import express from 'express'
 import knex from 'knex'
 import dotenv from 'dotenv'
+import createUser from './endpoints/createUser'
 
 dotenv.config()
 
-export const connection = knex({
-    client: 'mysql',
-    connection: {
-        host: process.env.DB_HOST, 
-        user: process.env.DB_USER, 
-        password: process.env.DB_PASSWORD, 
-        database: process.env.DB_NAME, 
-        port: 3306, 
-    }
+const connection = knex({
+ client: "mysql",
+ connection: {
+ host: process.env.DB_HOST,
+ port: 3306,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_DATABASE,
+ multipleStatements: true
+ }
 })
+
+export default connection
 
 const app = express()
 app.use(express.json())
+
+app.put('/user', createUser)
 
 app.listen(3003, () => {
     console.log("Server is running in 3003")
 })
 
-app.put('/', createUser)
+
